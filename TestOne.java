debea8279a934cff6203e0f4b544324ff3982393
fatsfa <fathifathima808@gmail.com>
2024-05-17 16:47:02 +0530

    first commit

---
+++ TestOne.java
@@ -0,0 +1,9 @@
+class Test{
+    public static void main(String[] args){
+
+        int[] eids = {100,200, 300};
+        for(int eid : eids){
+            System.out.println(eid);
+        }
+    }
+}
