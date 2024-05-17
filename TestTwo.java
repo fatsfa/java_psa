4c996a572223944c5c292ee75c207a0dbab5784b
fatsfa <fathifathima808@gmail.com>
2024-05-17 16:57:43 +0530

    tst

--- TestTwo.java
+++ TestTwo.java
@@ -1,6 +1,10 @@
+import java.util.Scanner;
 class Test{
     static int a;
     public static void main(String[] args){
+        Scanner s = new Scanner(System.in);
+        int b = s.nextInt();
+         System.out.println(b);
         System.out.println(a);
 
     }
