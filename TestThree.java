56983c29ec1031af4bac60516d9d44c88624f9c0
fatsfa <fathifathima808@gmail.com>
2024-05-21 12:17:53 +0530

    tst

--- TestThree.java
+++ TestThree.java
@@ -1,11 +1,16 @@
 class Test{
     public static void main(String[] args){
-        int[] arr = {1,2,3};
+        /*int[] arr = {1,2,3};
         for(int a : arr){
             System.out.println(a);
             System.out.println("Hy");
+        }*/
+        int i = 1;
+        while(i<=10){
+            if(i++ == 5)
+            continue;
+            System.out.println(i);
         }
-
         
     }
 }
