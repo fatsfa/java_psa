b521380f54f5a415be294e29772d2f647ff804d2
fatsfa <fathifathima808@gmail.com>
2024-05-23 18:45:13 +0530

    tst

--- access_modifier/final.java
+++ access_modifier/final.java
@@ -1,8 +1,14 @@
-final class A{
-    public static void main(String[] args){
+ class A{
+    final void m1(){
 
     }
+    public static void main(String[] args){
+        //  final int a = 100;
+        //  a = 200;
+        //  System.out.println(a);
+    }
 }
-class child extends A{
-    
-}
+ class child extends A{
+    void m1();
+ }
+
